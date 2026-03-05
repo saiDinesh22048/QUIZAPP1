55281606a83710da7f0c09c9eae583f60f59b4d9
TikamChandGupta <tikam_chandgupta@epam.com>
2026-03-05 22:32:20 +0530

    use hash based for admin page

--- qizzapp/frontend/src/App.jsx
+++ qizzapp/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { SocketProvider } from './context/SocketContext';
 import { AuctionProvider } from './context/AuctionContext';
 import { QuizProvider } from './context/QuizContext';
